63b66a4d1617b2af7f2a2b0a14f863691a45d203
WillMan11 <mangasw3659@student.faytechcc.edu>
2022-11-10 14:12:16 -0500

    P5LAB1 leap year stuff
    do in zybooks maybe

---
+++ P5LAB1_MangasW.py
@@ -0,0 +1,30 @@
+# CTI 110
+# P5LAB - Leap Year
+# MangasW
+# 10/11/2022
+def days_in_feb(year):
+  isLeap = False
+  # Can we divide evenly by four?
+  if year % 4 == 0: # it might be
+    isLeap = True
+    # if it's a century, must be divisible by 400
+    if year % 100 == 0:
+      if year % 400 == 0:
+        isLeap = True
+      else:
+        isLeap = False
+  if isLeap:
+    daysInFeb = 29
+  else:
+    daysInFeb = 28
+  print(year, "has", daysInFeb, "days in February.")
+        
+
+def main():
+  year = int(input("Enter a year: "))
+  days_in_feb(year)
+
+
+
+# start program
+main()
